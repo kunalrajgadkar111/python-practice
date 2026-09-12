174570ed5a10407955396152983ec6d2b68e47c5
Kunal Rajgadkar <kunalrajgadkar111@gmail.com>
2026-09-12 23:29:41 +0530

    Add Python for tuples  practice

---
+++ basics/tuples.py
@@ -0,0 +1,13 @@
+colors = ("red", "green", "blue", "yellow")
+
+print("Colors:", colors)
+
+print("First color:", colors[0])
+print("Last color:", colors[-1])
+
+print("First two colors:", colors[0:2])
+
+print("Number of colors:", len(colors))
+
+print("Is red present?", "red" in colors)
+print("Is black present?", "black" in colors)
