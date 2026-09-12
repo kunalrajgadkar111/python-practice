4331166b5d4d29a14a178fb87a811b52d6cb596e
Kunal Rajgadkar <kunalrajgadkar111@gmail.com>
2026-09-12 23:34:41 +0530

    Add Python for set operations practice

---
+++ basics/set_operations.py
@@ -0,0 +1,11 @@
+a = {1, 2, 3, 4, 5}
+b = {4, 5, 6, 7, 8}
+
+print("Set A:", a)
+print("Set B:", b)
+
+print("Union:", a | b)
+print("Intersection:", a & b)
+print("Difference A - B:", a - b)
+print("Difference B - A:", b - a)
+print("Symmetric Difference:", a ^ b)
