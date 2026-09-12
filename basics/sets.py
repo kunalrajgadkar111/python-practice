0299a2527b1d08257e6bb6cb984e8333f731b900
Kunal Rajgadkar <kunalrajgadkar111@gmail.com>
2026-09-12 23:32:21 +0530

    Add Python for sets practice

---
+++ basics/sets.py
@@ -0,0 +1,14 @@
+fruits = {"apple", "banana", "mango", "orange"}
+
+print("Fruits:", fruits)
+
+fruits.add("grapes")
+print("After add:", fruits)
+
+fruits.remove("banana")
+print("After remove:", fruits)
+
+print("Number of fruits:", len(fruits))
+
+print("Is mango present?", "mango" in fruits)
+print("Is watermelon present?", "watermelon" in fruits)
