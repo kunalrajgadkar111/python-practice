f48c26628a34893b6bad12e13976a9008f4aab7c
Kunal Rajgadkar <kunalrajgadkar111@gmail.com>
2026-09-12 23:47:09 +0530

    Add Python strings practice

---
+++ basics/strings.py
@@ -0,0 +1,20 @@
+name = "Kunal Rajgadkar"
+city = "Nagpur"
+course = "Python"
+
+print("Name:", name)
+print("City:", city)
+print("Course:", course)
+
+print("First character:", name[0])
+print("Last character:", name[-1])
+
+print("First five characters:", name[0:5])
+
+print("Length:", len(name))
+
+print("Uppercase:", name.upper())
+print("Lowercase:", name.lower())
+
+print("Is 'Kunal' present?", "Kunal" in name)
+print("Is 'Mumbai' present?", "Mumbai" in name)
