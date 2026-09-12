886e87ec0b22d727c62644ffc792ef8135321207
Kunal Rajgadkar <kunalrajgadkar111@gmail.com>
2026-09-12 22:05:32 +0530

    Add Python input output practice

---
+++ basics/input_output.py
@@ -0,0 +1,10 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+height = float(input("Enter your height in feet: "))
+
+print("\n--- Your Information ---")
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+
+print("\nAge next year:", age + 1)
